9c7463f19f24d73dba6cefeec274eb54aaa58b99
Benjamin Danishevsky <ben.danishevsky@gmail.com>
2022-04-04 19:00:51 -0400

    db models/migrations

---
+++ backend/db/migrations/20220404224947-create-note.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Notes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users' }
+      },
+      notebookId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Notesbooks' }
+      },
+      title: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Notes');
+  }
+};
