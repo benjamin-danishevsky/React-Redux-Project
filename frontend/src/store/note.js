cfb0b9d4f65bf224e6d9bde6ada7f0b48007791f
Benjamin Danishevsky <ben.danishevsky@gmail.com>
2022-04-08 19:35:37 -0400

    notes reducer/thunks added

--- frontend/src/store/note.js
+++ frontend/src/store/note.js
@@ -2,8 +2,8 @@ import { csrfFetch } from './csrf';
 
 const GET_NOTES = '/notes/GET_NOTES';
 const CREATE_NOTE = '/notes/CREATE_NOTE';
-const UPDATE_NOTES = '/notes/UPDATE_NOTES';
-const DELETE_NOTES = '/notes/DELETE_NOTES';
+const UPDATE_NOTE = '/notes/UPDATE_NOTES';
+const DELETE_NOTE = '/notes/DELETE_NOTES';
 
 //action creators
 export const getNotes = (notes) => {
@@ -43,5 +43,74 @@ export const getNoteThunk = (userId) => async (dispatch) => {
     if(response.ok){
         const notes = await response.json();
         dispatch(getNotes(notes.notes))
+    }
+}
+
+//create note thunk
+export const postNoteThunk = (note, userId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/users/${userId}/notes`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(note)
+    });
+    if(response.ok){
+
+        const newNote = await response.json()
+
+        dispatch(createNotebook(newNote))
+        return newNote
+    }
+
+}
+
+//delete note thunk
+
+export const deleteNoteThunk = (noteId, userId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/users/${userId}/notes/${noteId}`, {
+      method: 'DELETE',
+    });
+    if (response.ok) {
+        const removedNote = await response.json();
+        dispatch(deleteNotebook(removedNote));
+        return removedNote
+    }
+}
+
+//update note thunk
+export const editNoteThunk = (updatedNote) => async (dispatch) => {
+    const { userId, noteId, title, content } = updatedNote;
+    const response = await csrfFetch(`/api/users/${userId}/notes/${noteId}`, {
+      method: 'PUT',
+      body: JSON.stringify({ userId, noteId, title, content})
+    });
+    if (response.ok) {
+        const editedNote = await response.json();
+        dispatch(updateNote(editedNote));
+        return editedNote
+    }
+}
+
+const initialState = {};
+
+const noteReducer = (state = initialState, action) => {
+    let newState;
+    switch(action.type){
+        case GET_NOTES:
+            newState = {...state};
+            action.notes.forEach(note => newState[note.id] = note);
+            return newState;
+        case CREATE_NOTE:
+            newState = {...state};
+            newState[action.newNote.newNote.id] = action.newNote.newNote
+            return newState;
+        case UPDATE_NOTE:
+            newState = {...state};
+            newState[action.note.id] = note
+        case DELETE_NOTE:
+            newState = {...state};
+            delete newState[action.note.deletedNote.id]
+            return newState;
+        default:
+            return state;
     }
 }
